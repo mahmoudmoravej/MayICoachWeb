a4b7f9f9eba8969d12643dfb5c43af7be868fd31
Mahmoud Moravej <moravej@gmail.com>
2023-11-11 00:11:02 -0500

    init testing - vitest

---
+++ app/routes/managers/tests/route.test.tsx
@@ -0,0 +1,30 @@
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { createRemixStub } from "@remix-run/testing";
+import { render, screen, waitFor } from "@testing-library/react";
+import { it, describe } from "vitest";
+
+describe("Group of tests", () => {
+  it("renders loader data", async () => {
+    // ⚠️ This would usually be a component you import from your app code
+    function MyComponent() {
+      const data = useLoaderData() as { message: string };
+      return <p>Message: {data.message}</p>;
+    }
+
+    const RemixStub = createRemixStub([
+      {
+        path: "/",
+        Component: MyComponent,
+        loader() {
+          return json({ message: "hello" });
+        },
+      },
+    ]);
+
+    render(<RemixStub />);
+
+    await waitFor(() => screen.findByText("Message: hello"));
+    // expect(screen.getByText("Message: hello")).not.to.be.null;
+  });
+});
