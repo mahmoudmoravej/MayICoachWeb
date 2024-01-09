c2dd96e6673de47d09c8d758d4fe87faf6b9cad1
Mahmoud Moravej <55846417+mahmoudmoravej@users.noreply.github.com>
2024-01-09 14:04:51 -0500

    remove unnecessary graphql server headers which causes error in production (#38)

--- app/utils/graphql.ts
+++ app/utils/graphql.ts
@@ -1,18 +1,23 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
 
 export function getApolloClient(request: Request, token: string | undefined) {
+  const linkSettings = {
+    uri: process.env.GRAPHQL_SCHEMA_URL || "GRAPHQL_SCHEMA_URL IS NOT SET",
+    headers: {
+      // ...Object.fromEntries(request.headers),
+      Authorization: `Bearer ${token ?? "ERROR TOKEN!"}`,
+    },
+    credentials: "include", // or "same-origin" if your backend server is the same domain
+  };
+
+  console.log("linkSettings", linkSettings);
+
   const client = new ApolloClient({
     ssrMode: true,
     cache: new InMemoryCache(),
-    link: createHttpLink({
-      uri: process.env.GRAPHQL_SCHEMA_URL || "GRAPHQL_SCHEMA_URL IS NOT SET",
-      headers: {
-        ...Object.fromEntries(request.headers),
-        Authorization: `Bearer ${token ?? "ERROR TOKEN!"}`,
-      },
-      credentials: request.credentials ?? "include", // or "same-origin" if your backend server is the same domain
-    }),
+    link: createHttpLink(linkSettings),
   });
+
   return client;
 }
 
