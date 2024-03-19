2b45475d5a597ec20e5e9f48dfa1f4fcdedf46cf
Mahmoud Moravej <moravej@gmail.com>
2024-03-18 22:01:35 -0400

    add github review

--- app/routes/_dashboard.individuals.($id).activities/route.tsx
+++ app/routes/_dashboard.individuals.($id).activities/route.tsx
@@ -79,6 +79,7 @@ export default function Activities() {
     date: new Date(node.date),
     isAnalyzed: node.isAnalyzed,
     cycle: node.cycle?.title ?? "-",
+    channelId: node.channelId,
   }));
 
   const cycles = data.cycles?.nodes?.filter(noNull).map((node) => ({
@@ -203,7 +204,10 @@ export default function Activities() {
             </thead>
             <tbody>
               {activities.map(
-                ({ title, id, url, isAnalyzed, date, cycle }, index) => {
+                (
+                  { title, id, url, isAnalyzed, date, cycle, channelId },
+                  index,
+                ) => {
                   const isLast = index === activities.length - 1;
                   const classes = isLast
                     ? "p-4"
@@ -213,16 +217,28 @@ export default function Activities() {
                     <tr key={id}>
                       <td className={classes}>
                         <div className="flex items-center gap-3">
-                          <svg
-                            height="32"
-                            aria-hidden="true"
-                            viewBox="0 0 16 16"
-                            version="1.1"
-                            width="32"
-                            data-view-component="true"
-                          >
-                            <path d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z"></path>
-                          </svg>
+                          {channelId == 1 ? (
+                            <svg
+                              width="24"
+                              height="24"
+                              aria-hidden="true"
+                              viewBox="0 0 16 16"
+                              version="1.1"
+                              data-view-component="true"
+                            >
+                              <path d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z"></path>
+                            </svg>
+                          ) : (
+                            <svg
+                              width="24"
+                              height="24"
+                              data-view-component="true"
+                              version="1.1"
+                              viewBox="0 0 1792 1792"
+                            >
+                              <path d="M704 1216q0 40-12.5 82t-43 76-72.5 34-72.5-34-43-76-12.5-82 12.5-82 43-76 72.5-34 72.5 34 43 76 12.5 82zm640 0q0 40-12.5 82t-43 76-72.5 34-72.5-34-43-76-12.5-82 12.5-82 43-76 72.5-34 72.5 34 43 76 12.5 82zm160 0q0-120-69-204t-187-84q-41 0-195 21-71 11-157 11t-157-11q-152-21-195-21-118 0-187 84t-69 204q0 88 32 153.5t81 103 122 60 140 29.5 149 7h168q82 0 149-7t140-29.5 122-60 81-103 32-153.5zm224-176q0 207-61 331-38 77-105.5 133t-141 86-170 47.5-171.5 22-167 4.5q-78 0-142-3t-147.5-12.5-152.5-30-137-51.5-121-81-86-115q-62-123-62-331 0-237 136-396-27-82-27-170 0-116 51-218 108 0 190 39.5T603 419q147-35 309-35 148 0 280 32 105-82 187-121t189-39q51 102 51 218 0 87-27 168 136 160 136 398z"></path>
+                            </svg>
+                          )}
                           <div className="flex flex-col">
                             <Link
                               to={`/activities/${id}/edit`}
@@ -239,7 +255,7 @@ export default function Activities() {
                               variant="small"
                               className="font-normal italic"
                             >
-                              Pull Request Contribution
+                              {channelId == 1 ? "Contribution" : "Reviewed"}
                             </Typography>
                           </div>
                         </div>
